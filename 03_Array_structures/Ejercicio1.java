7f8db3f506c1919872093c1d17127abb081fb97d
Eduardo Lucas <eduardolucasmunozdelucas@gmail.com>
2025-01-23 23:48:15 +0100

    Update Ejercicio1.java
    Reorganización de comentarios

--- 03_Array_structures/Ejercicio1.java
+++ 03_Array_structures/Ejercicio1.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.actividad2uf1tema3;
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
  *
- * @author eduardolucasmunozdelucas
+ * @author edulumulu
  */
 public class Ejercicio1 {
 
@@ -20,24 +14,25 @@ public class Ejercicio1 {
 
         for (int i = 0; i < notas.length; i++) {    //Utilizo un bucle para solicitar al usuario las 10 notas
             try {
-                System.out.println("Escribe la nota " + (i + 1) + " :"); //pido las notas desde la nº1 a la10
-                notas[i] = sc.nextDouble();                //Guardar el valor introducido en cada espacio del arrays 
+                System.out.println("Escribe la nota " + (i + 1) + " :"); 
+                notas[i] = sc.nextDouble();                //Guardar el valor introducido en cada espacio del array 
+
+                // Condición de un número entre 0 y 10
                 if (notas[i] < 0 | notas[i] > 10) {
-                    //En caso de de que el valor sea un double pero no está entre 0 y 10 aviso al cliente y le pido que meta un valor valido
                     System.out.println("La nota introducida no es correcta. El valor debe estar entre 0 y 10 ");
-                    i--;        //Como ha introducido un valor no válido resto una iteración para que vuelva a pedirle el valor erroneo y no el siguiente
+                    i--;        // Resto una iteración para que vuelva a pedirle el valor para la misma posición del array 
                 }
-            } catch (InputMismatchException e) {          //Introduzco una excepción por si introducen por teclado valor no válido
+            } catch (InputMismatchException e) {          //controlo la esxecepcion de caracter no válido
                 System.out.println("La entrada no es valida, debe ser un número entre 0 y 10");
-                sc.next();      //Para que no se quede bloqueado el escaner en caso de que salte el catch
-                --i;         //Como ha introducido un valor no válido resto una iteración para que vuelva a pedirle el valor erroneo y no el siguiente
-
+                sc.next();       // Doy paso para la siguiente introducción de caracteres
+                --i;             // Resto una iteración para que vuelva a pedirle el valor para la misma posición del array 
             }
-
         }
+        
         System.out.println(" ");
         System.out.println("Tus notas son: ");
-        //A la hora de mostrar los valores por pantalla hago un for con los condicionales de cada nota válida introducida
+        
+        // Recorro el Array y dependiendo del valor de la nota incluyo un texto
         for (int i = 0; i < notas.length; i++) {
             if (notas[i] >= 0 && notas[i] < 3) {
                 System.out.println("Nota: "+notas[i] + " - Deficiente");
@@ -50,43 +45,6 @@ public class Ejercicio1 {
             } else {
                 System.out.println("Nota: "+notas[i] + " - Excelente");
             }
-
         }
-
     }
 }
-
-//También podría hacer esta estructura con un switch
-//        System.out.println(" ");
-//        System.out.println("Opción 2 --> estructura con switch - cases: ");
-
-//        for (int i = 0; i < notas.length; i++) {
-//            switch ((int) notas[i]) {   //Para ello hago una conversión explícita ya que solo puede evaluar expresiones que sean tipos primitivos enteros
-//                case 0:
-//                case 1:
-//                case 2:
-//                    System.out.println("Nota: "+notas[i] + " - Deficiente");
-//                    break;     //Para que no siga recorriendo el resto de cases y haga la sigiente iteración del bucle
-//                case 3:
-//                case 4:
-//                    System.out.println("Nota: "+notas[i] + " - Insuficiente");
-//                    break;      
-//                case 5:
-//                case 6:
-//                case 7:
-//                    System.out.println("Nota: "+notas[i] + " - Bien");
-//                    break;      
-//                case 8:
-//                case 9:
-//                    System.out.println("Nota: "+notas[i] + " - Notable");
-//                    break;      
-//                case 10:
-//                    System.out.println("Nota: "+notas[i] + " - Excelente");
-//                    break;
-                  /*En este caso no sería necesario el defalult ya que debido a la preveción de errores cuando solictamos las notas, 
-                    nunca podría tener un valor que no estuviera dentro de los cases señalados*/  
-//                default:
-//                    System.out.println("Nota no válida");
-//                    break;
-//            }
-//        }
