a186f1f75e4c30e81c8568d802fdbb4e02c0789a
Eduardo Lucas <eduardolucasmunozdelucas@gmail.com>
2025-01-24 15:20:28 +0100

    Update IntroduccionDatos.java

--- 05._Files_write_read_create_delete/IntroducciónDatos/IntroduccionDatos.java
+++ 05._Files_write_read_create_delete/IntroducciónDatos/IntroduccionDatos.java
@@ -6,15 +6,20 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
- * @author eduardolucasmunozdelucas
- * Clase que utilizo para crear funciones que restrinjan los datos introducidos por el teclado
+ *
+ * @author edulumulu
  */
 public class IntroduccionDatos {
 
+    /**
+     * Pide al usuario un número que esté entre los valores a y b
+     * @param texto
+     * @param a
+     * @param b
+     * @return 
+     */
     public static int numeroEntreDosOpciones(String texto, int a, int b) {
-        /*
-        FUncion que recibe un string como linea par mostrar por pantalla y dos numeros enteros, haciendo un return de un entero
-        */
+     
         Scanner sc = new Scanner(System.in);    //Objeto Scanner 
         boolean ok = true;                      //inicializo variable boleana para bucle while
         int n = 0;                              //inicializo variable que se modificará una vez reciba el valor por teclado
@@ -22,18 +27,18 @@ public class IntroduccionDatos {
         while (ok) {
             try {
                 System.out.println(texto);      //Muestro texto
-                n = sc.nextInt();               //modifico valor de n al valor introducido
+                n = sc.nextInt();               //Igualo n al valor introducido por el usuario
 
                 if (n >= a && n <= b) {         //Si n mayor o igual que a y menor o igual que b (si cumple la restriccion) cambio la boleana para que terminte el bucle
                     ok = false;
-                } else {        //Si no cumple muestro mensaje de que no es correcto y vuelvo a mostrar el menú para que elija de nuevo
+                } else {                        //Si no cumple muestro mensaje de que no es correcto y vuelvo a mostrar el menú para que elija de nuevo
                     System.out.println("El dato introducido es incorrecto, debe de ser un numero entero del 1 al 6.");
                     System.out.println("Vuelve a intentarlo.");
                     System.out.println(" ");
                     menuGeneral("Opciones:", "(1) Crea un nuevo Archivo. ", "(2) Lista los archivos de la carpeta.", "(3) Muestra un archivo. ",
                             "(4) Borra un archivo.", "(5) Renombra un archivo", "(6) Sal del programa ");
                 }
-            } catch (InputMismatchException e) {    //En caso de introducir caractres no numéricos salata la excepción mostrando mensaje y nuevamente el menú
+            } catch (InputMismatchException e) {        //Controlo la excepción en caso de introducir caractres no numéricos 
                 System.out.println("El dato introducido es incorrecto, debe de ser un numero entero del 1 al 6.");
                 System.out.println("Vuelve a intentarlo");
                 System.out.println(" ");
@@ -42,33 +47,7 @@ public class IntroduccionDatos {
                 sc.next();
             }
         }
-        return n;       //Por último retorno el valor de la variable de retorno
-    }
-
-    
-    //Esta función al final no la utilicé lo creé para elegir un número comprendido entre la cantidad de posiciones de un array
-    public static int introducirNumero(String[] a) {
-        Scanner sc = new Scanner(System.in);
-        int n = 0;
-        boolean ok = true;
-        while (ok) {
-            try {
-                System.out.println("Elige un archivo (escribe el número correspondiente):");
-                n = sc.nextInt();
-                if (n >= 1 && n <= a.length) {
-                    ok = false;
-                    return n;
-
-                } else {
-                    System.out.println("El numero introducido debe de estar entre 1 y " + a.length);
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("El numero introducido debe de estar entre 1 y " + a.length);
-            }
-
-        }
-
-        return n;
+        return n;       //retorno n
     }
 
 }
