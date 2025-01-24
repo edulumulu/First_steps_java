a787073170f1356a4018a46b57f8b718935392c0
Eduardo Lucas <eduardolucasmunozdelucas@gmail.com>
2025-01-24 15:39:42 +0100

    Update Posibilidades.java

--- 05._Files_write_read_create_delete/PosibilidadesPrograma/Posibilidades.java
+++ 05._Files_write_read_create_delete/PosibilidadesPrograma/Posibilidades.java
@@ -1,6 +1,5 @@
 package PosibilidadesPrograma;
 
-//Los dos primeros import trabajan con funciones que he creado yo, el resto son para el tratamiento de files, escritura, lectura, etc. Scanner y Excepcion Input output
 import static IntroducciónDatos.Rutas.*;
 import static MostrarPorConsola.Menues.menuGeneral;
 import java.io.BufferedReader;
@@ -12,20 +11,28 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /**
- * @author eduardolucasmunozdelucas En esta clase he reunido todas las funciones
- * relacionadas con las posibilidades del menú incluyendo la de crear una
+ * @author edulumulu
  * carpeta
  */
 public class Posibilidades {
 
-    public static void carpetaArchivos() {      //Función que crea carpeta 
+    /**
+     * crea una carpeta en la rita predeterminada
+     */
+    public static void carpetaArchivos() {      
         File carpeta = new File(rutaCarpetaArchivos());
         if (carpeta.mkdir() == true) {
-            System.out.println("Carpeta -Archivos- creada con éxito");  //En caso de que no existiera antes muestra por pantalla mensaje, si no no muestra nada
+            System.out.println("Carpeta -Archivos- creada con éxito");  
         }
 
     }
 
+    
+    /**
+     * @author eduardolucasmunozdelucas En esta clase he reunido todas las funciones
+     * relacionadas con las posibilidades del menú incluyendo la de crear una
+     * carpeta
+     */
     public static String nuevoArchivo() {
         Scanner sc = new Scanner(System.in);
         String nombreArchivo = "Vacío";     //Doy un nombreopcional al archivo que modificaré cuando el usuario lo modifique
@@ -72,21 +79,25 @@ public class Posibilidades {
         return nombreArchivo;
     }
 
+    /**
+     * Lista por pantalla los archivos y retorna un array con las rutas de los archivos
+     * @return 
+     */
     public static String[] listarArchivos() {  //Funcion que ademas de listar por pantalla el nombre de los archivos numerados, retorna un array con sus rutas
 
-        String rutaCarpeta = rutaCarpetaArchivos(); //Obengo ruta de carpeta
+        String rutaCarpeta = rutaCarpetaArchivos();     //Obengo ruta de carpeta
         File carpetaArchivos = new File(rutaCarpeta);   //creo File con esa ruta
 
         if (carpetaArchivos.exists() && carpetaArchivos.isDirectory()) {        //si existe la ruta y es un directorio paso a lo siguiente
-            File[] archivos = carpetaArchivos.listFiles();      //Creo un Array de File llamado archivo  que sea el listado de la carpeta archivos
-            if (archivos != null) {                 //Si el contenido es diferente a null lo siguiente
-                if (archivos.length > 0) {          //Si la longitud del array es mayor que 0 lo siguiente
-                    String[] listadoRutasArchivos = new String[archivos.length];    //Creo un array de strings con la misma longitud del listado de Files
+            File[] archivos = carpetaArchivos.listFiles();                      //Creo un Array de File llamado archivo  que sea el listado de la carpeta archivos
+            if (archivos != null) {                                                         //Si el contenido es diferente a null lo siguiente
+                if (archivos.length > 0) {                                                  //Si la longitud del array es mayor que 0 lo siguiente
+                    String[] listadoRutasArchivos = new String[archivos.length];            //Creo un array de strings con la misma longitud del listado de Files
                     System.out.println("Listado:");
-                    for (int i = 0; i < archivos.length; i++) {     //Bucle for para recorrer el array 
+                    for (int i = 0; i < archivos.length; i++) {                             //Bucle for para recorrer el array 
                         if (archivos[i].isFile()) {
                             System.out.println((i + 1) + " - " + archivos[i].getName());    //imprimo a cada iteracion el nombre del archivo de la ruta recorrida del listado File
-                            listadoRutasArchivos[i] = archivos[i].getPath();        // Otorgo la ruta al String listadoRutasArchivos en la misma posicion que el listado File
+                            listadoRutasArchivos[i] = archivos[i].getPath();                // Otorgo la ruta al String listadoRutasArchivos en la misma posicion que el listado File
                         }
                     }
                     return listadoRutasArchivos;        //Una vez recorrido el listado entero y obtenidas todas las direcciones retorno el array
@@ -95,52 +106,53 @@ public class Posibilidades {
                     System.out.println("Antes de usar esta opción debes de crear archivos.");
                     return new String[0];
                 }
-            } else {    //En caso de que no hubiera archivos mensaje de error 
+            } else {                        //En caso de que no hubiera archivos mensaje de error 
                 System.out.println("Esto no es un directorio");
                 return new String[0];       //mensaje de error y retorna un array con 0
             }
-        } else {        //En caso de que la carpeta no existiera, cosa qeu no puede pasar por que la creo al principio en el main, pero bueno
+        } else {                            //En caso de que la carpeta no existiera, cosa qeu no puede pasar por que la creo al principio en el main, pero bueno
             System.out.println("La carpeta Archivos no existe");
-            return new String[0];       //mensaje de error y retorna un array con 0
+            return new String[0];           //mensaje de error y retorna un array con 0
         }
     }
 
+    /**
+     * Muestra el conteido del archivo selecionado del menú de archivos dentro de la carpeta
+     */
     public static void mostrarArchivo() {
         Scanner sc = new Scanner(System.in);
         boolean ok = true;
 
         while (ok) { // Bucle while
             try {
-                String[] listado = listarArchivos(); // Creo un array de string que sea igual que el que retorna de la función lista de archivos
+                String[] listado = listarArchivos();         // Creo un array de string que sea igual que el que retorna de la función lista de archivos
 
-                if (listado.length > 0) { // Si la longitud del array es mayor que 0, es decir, si hay archivos en la carpeta archivos lo siguiente
+                if (listado.length > 0) {                    // Si la longitud del array es mayor que 0, es decir, si hay archivos en la carpeta archivos lo siguiente
                     System.out.println("Elige el archivo que quieras leer:");
 
-                    int num = sc.nextInt(); // num igual que el valor introducido por teclado
+                    int num = sc.nextInt();                 // num igual que el valor introducido por teclado
 
-                    if (num < 1 || num > listado.length) { // Si el número no está entre 1 y la longitud del array, la opción no es válida
+                    if (num < 1 || num > listado.length) {                     // Si el número no está entre 1 y la longitud del array, la opción no es válida
                         System.out.println("La opción elegida no es válida");
                         System.out.println("El número tiene que estar comprendido entre las opciones señaladas:");
                         ok = true;      
-                        /*Lo pongo porque si no ponía nada al ejecutar el programa despues de mostrarme mensaje anterior esperaba 
-                        a que introduciera algun dato antes de volver a mostrar listado*/
                         
-                    } else { // En caso de que esté dentro de los valores comprendidos en la longitud del array lo siguiente
-                        // Muestro la ruta de la posición del array del número pedido al usuario menos 1 ya que los arrays empiezan por 0
+                    } else {                                 // En caso de que esté dentro de los valores comprendidos en la longitud del array lo siguiente
+                                                            // Muestro la ruta de la posición del array del número pedido al usuario menos 1 ya que los arrays empiezan por 0
                         System.out.println("La ruta del documento elegido es: " + listado[num - 1]);
 
-                        File seleccionado = new File(listado[num - 1]); // Creo el File con la ruta correspondiente
-                        FileReader fr = new FileReader(seleccionado); // Creo el File Reader para poder leer el documento
-                        BufferedReader br = new BufferedReader(fr); // Creo el BR para poder leer conjuntos de caracteres
+                        File seleccionado = new File(listado[num - 1]);           // Creo el File con la ruta correspondiente
+                        FileReader fr = new FileReader(seleccionado);             // Creo el File Reader para poder leer el documento
+                        BufferedReader br = new BufferedReader(fr);               // Creo el BR para poder leer conjuntos de caracteres
                         System.out.println("El contenido del documento es:");
                         for (String linea = br.readLine(); linea != null; linea = br.readLine()) { // Bucle for para recorrer todas las líneas del contenido
-                            System.out.println(linea); // Muestro por consola el texto leído línea a línea
+                            System.out.println(linea);                            // Muestro por consola el texto leído línea a línea
                         }
-                        br.close(); // Cierro el buffer
+                        br.close(); 
                         ok = false;
                     }
                 }else{
-                    ok= false;      //Para salir del bucle y por tanto de la opción menú
+                    ok= false;     
                 }
 
             } catch (java.util.InputMismatchException | java.lang.ArrayIndexOutOfBoundsException | IOException ex) {
@@ -148,11 +160,14 @@ public class Posibilidades {
                 System.out.println(" ");
                 System.out.println("La opción elegida no es válida.");
                 System.out.println("El número tiene que estar comprendido entre las opciones señaladas:");
-                sc.next(); // Siguiente buffer para que no se quede hasta el infinito
+                sc.next();         
             }
         }
     }
 
+    /**
+    * Elimina un archivo del menú de archivos de la carpeta
+    */
     public static void borrarArchivo() {
         Scanner sc = new Scanner(System.in);
         boolean ok = true;
@@ -175,25 +190,25 @@ public class Posibilidades {
                         System.out.println("La ruta del documento elegido es: " + listado[num - 1]);
                         menuGeneral("¿Quieres confirmar que se borre? ", "(1) Si. ", "(2) No, quiero borrar otro archivo", "(3) No, quiero volver al menú prnincipal");
                         int respuesta = sc.nextInt();
-                        if (respuesta == 1) {           //Con una estructura condicional (por cambiar) si el usiario elige la opción uno se intenta borrar el archivo
+                        if (respuesta == 1) {               //Con una estructura condicional (por cambiar) si el usiario elige la opción uno se intenta borrar el archivo
                             File elegido = new File(listado[num - 1]);
                             try {
                                 if (elegido.delete()) {
-                                    System.out.println("Has eliminado el archivo"); //En caso de haberlo borrado correctamente
-                                } else {        //En caso de que no se haya podido borrar te avisa
+                                    System.out.println("Has eliminado el archivo");         //En caso de haberlo borrado correctamente
+                                } else {                                                    //En caso de que no se haya podido borrar te avisa
                                     System.out.println("No se pudo eliminar el archivo");
                                 }
-                                ok = false;         //modifico la booleana para que se salga del bucle
-                            } catch (SecurityException e) {     //contemplo la excepción de avisar si no tiene permisos para borrar el archivo
+                                ok = false;                                 //modifico la booleana para que se salga del bucle
+                            } catch (SecurityException e) {                 //contemplo la excepción de avisar si no tiene permisos para borrar el archivo
                                 System.out.println("No tienes permisos para borrar este archivo");
                             }
-                        } else if (respuesta == 3) {    //En caso de que el usuario se arrepienta de borrar archivos
-                            ok = false;                 //modifico la booleana para que se salga del bucle
-                        } else {           //En caso de que no quiera borrar el archivo en concreto regresa al menú que le muestra las opciones de archivos a borrar
+                        } else if (respuesta == 3) {                        //En caso de que el usuario se arrepienta de borrar archivos
+                            ok = false;                                     //modifico la booleana para que se salga del bucle
+                        } else {                                           //En caso de que no quiera borrar el archivo en concreto regresa al menú que le muestra las opciones de archivos a borrar
                             System.out.println("No se ha borrado el archivo, vuelve a intentarlo");
                         }
 
-                    } else {            //Controla que si el numero no esta dentro del rango indicado mustra una alerta 
+                    } else {                    //Controla que si el numero no esta dentro del rango indicado mustra una alerta 
                         System.out.println("La opción elegida no es válida");
                         
                     }
@@ -201,16 +216,19 @@ public class Posibilidades {
                     System.out.println(" ");
                     System.out.println("La opción elegida no es válida.");
                     System.out.println("El numero tiene que estar comprendido entre las opciones señaladas:");
-                    sc.next();      //Siguiente bufer para que no se quede hasta el infinito
+                    sc.next();      
                 }
 
-            } else {        //En caso de que no haya archivos dentro de la carpeta sale del bucle y del método
+            } else {        
                 ok = false;
             }
         }
 
     }
 
+    /**
+    * Permite selecionar un archivo de la lista de archivos de la carpeta y renombrarlo.
+    */
     public static void renombrarArchivo() {
         /*
         Metodo totalmente simitlar al anterior (BorrarArchivo). Lo único que en este no he contemplado un menú que pregunta
